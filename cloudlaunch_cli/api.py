5a2f0dd0be992944c7ff53179b4817f7c1c22d2e
Marcus Christie <machrist@iu.edu>
2018-03-16 11:19:06 -0400

    Refactored how to figure out parent_url_kwargs

--- cloudlaunch_cli/api.py
+++ cloudlaunch_cli/api.py
@@ -22,35 +22,36 @@ class APIClient:
 
 class APIResponse:
 
-    def __init__(self, id, data=None, parent=None):
+    def __init__(self, id, data=None):
         self.id = id
         self.data = data
-        self.parent = parent
 
 
 class APIEndpoint:
 
     path = None
     subroutes = {}
+    id_param_name = 'id'
     parent_url_kwarg = None
-    parent_endpoint = None
 
-    def __init__(self, api_config, parent=None, parent_endpoint=None):
+    def __init__(self, api_config, parent_id=None, parent_url_kwargs=None):
         self.api_config = api_config
-        self.parent = parent
-        self.parent_endpoint
+        self.parent_url_kwargs = parent_url_kwargs if parent_url_kwargs else {}
+        # TODO: maybe warn if parent_id is specified but not parent_url_kwarg
+        if parent_id and self.parent_url_kwarg:
+            self.parent_url_kwargs[self.parent_url_kwarg] = parent_id
 
     def get(self, id, **kwargs):
         document = self._create_client()
         params = kwargs if kwargs else {}
-        params['id'] = id
-        if self.parent:
-            params.update(self._get_parent_url_kwargs(self.parent, self))
+        params[self.id_param_name] = id
+        if self.parent_url_kwargs:
+            params.update(self.parent_url_kwargs)
         resp = self._client.action(document, self.path + ['read'], params=params)
-        api_response = APIResponse(id=id, data=resp, parent=self.parent)
+        api_response = APIResponse(id=id, data=resp)
         if self.subroutes:
             for k, v in self.subroutes.items():
-                setattr(api_response, k, v(self.api_config, parent=api_response, parent_endpoint=self))
+                setattr(api_response, k, v(self.api_config, parent_id=id, parent_url_kwargs=self.parent_url_kwargs))
         return api_response
 
     def add_subroute(self, name, child_endpoint):
@@ -70,18 +71,6 @@ class APIEndpoint:
         ]
         self._client = coreapi.Client(transports=custom_transports)
         return self._client.get('{url}/schema/'.format(url=url))
-
-    # TODO: resolve parent url kwargs when creating subroute endpoint instances
-    # this way the subroute endpoint instances don't know anything about the data
-    # don't technically need to know about parent_endpoint either???
-    # - a endpoint could have parent_url_kwargs?
-    def _get_parent_url_kwargs(self, parent_api_response, api_endpoint):
-        kwargs = {}
-        if parent_api_response:
-            kwargs[api_endpoint.parent_url_kwarg] = parent_api_response.id
-            if parent_api_response.parent:
-                kwargs.update(self._get_parent_url_kwargs(parent_api_response, api_endpoint.parent_endpoint))
-        return kwargs
 
 
 class Deployments(APIEndpoint):
